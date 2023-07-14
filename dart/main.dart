ea49dc3dc6f6a08dab89090ded4af2aa7e235934
tommy@laptop <tommy.dm.kim@gmail.com>
2023-07-14 22:11:50 +0900

    initial commit

---
+++ dart/main.dart
@@ -0,0 +1,16 @@
+void main() {
+  // hello world
+  print("hello world!");
+
+  // variables
+  var name = "dongmin";
+  String name2 = "dongmin2";
+  dynamic named;
+  String? nico = "nico";
+  nico = null;
+  nico?.length;
+  final namef = "dong";
+  late final String namelf;
+  const API = "121212";
+  
+} 
